aff82042e003d0c9fdaa6e31de896ab9714e47a2
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-08-27 20:20:31 +0600

    added new middleware varifyAdmin and varifyToken

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -30,11 +31,61 @@ async function run() {
     const reviewsCollection = client.db("BistroDB2").collection("reviews");
     const cartCollection = client.db("BistroDB2").collection("cart");
 
-    // users api
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "8h",
+      });
+      res.send({ token });
+    });
 
-    app.get("/users", async (req, res) => {
+    // middlewarres
+    const varifyToken = (req, res, next) => {
+      console.log("inside varified token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorired access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    // use verify admin after verifytoken
+    const varifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+    // users api
+    app.get("/users", varifyToken, varifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       return res.send(result);
+    });
+
+    app.get("/users/admin/:email", varifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
     });
 
     app.post("/users", async (req, res) => {
