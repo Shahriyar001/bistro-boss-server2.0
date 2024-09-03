58a7629e37b2e6e9039bba6639771f01c8512a06
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-09-03 22:48:11 +0600

    added vercel.json file

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const usersCollection = client.db("BistroDB2").collection("users");
     const menuCollection = client.db("BistroDB2").collection("Menu");
@@ -432,10 +432,10 @@ async function run() {
     // carts collections
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
